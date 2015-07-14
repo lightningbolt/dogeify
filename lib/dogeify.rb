0ea42e57eea9b4fc8c13aed9e4ae7dd8dc1fe22c
David Chang <david.chang@criticalmedia.com>
2015-07-14 20:19:45 +0000

    changed sentence and word parsing. added dogeifying of other parts of speech: adjectives, adverbs, and infinitive verbs. improved selection of doge adjectives.

--- lib/dogeify.rb
+++ lib/dogeify.rb
@@ -3,40 +3,85 @@ require 'engtagger'
 
 class Dogeify
   ADJECTIVES = %w[so such very much many how].freeze
+  ADJECTIVE_MAP = {:adjective => %w[such many],
+                   :singular_noun => %w[so such very much many how],
+                   :plural_noun => %w[so such very much many how],
+                   :infinitive_verb => %w[so such very much many how],
+                   :adverb => %w{such very much many how}}.freeze
   EMOTIONS = %w[wow amaze excite].freeze
+  IGNORE_PATTERNS = %w[i im be do have] + ADJECTIVES + EMOTIONS
+  TAGS = {:jj => :adjective,
+    :jjr => :adjective,
+    :jjs => :adjective,
+    :nn => :singular_noun,
+    :nnp => :singular_noun,
+    :nnps => :plural_noun,
+    :nns => :plural_noun,
+    :rbr => :adverb,
+    :rbs => :adverb,
+    :vb => :infinitive_verb}.freeze
 
   def initialize
     @tagger = EngTagger.new
+    @adjectives = ADJECTIVES.dup
+    @pos_adjectives = {}
+    @emotions = []
   end
 
   def process(str, options = {})
+    # Remove punctuation and extra whitespace.
+    str = str.strip.gsub(/\s+/, " ").gsub(/[\:"\(\)\{\}]+/, " ").gsub("'", "")
     # Parse sentences.
     sentences = str.downcase.split(/[\.!?]+/).map(&:strip)
-
+    ignore = IGNORE_PATTERNS | (options[:ignore] || [])
     sentences = sentences.map do |sentence|
-      # Ignore any provided patterns.
-      sentence = ignore_patterns(sentence, options[:ignore]) if options[:ignore]
-      
-      # Select just the nouns.
-      tagged_sentence = tagger.add_tags(sentence)
-      phrases = tagger.get_nouns(tagged_sentence).keys rescue []
+      sentence = ignore_patterns(sentence, ignore)
+      tagged_sentence = tagger.add_tags(sentence) || next
+      translated_phrases = []
+      tagged_sentence.scan(/<.+?>.+?<\/.+?>/).each do |phrase|
+        tag = phrase.scan(/<(.+?)>.+$/).flatten.first
+        if (pos = part_of_speech(tag))
+          word = phrase.scan(/<.+?>(.+?)<\/.+?>/).flatten.first
+          translated_phrases << "#{adjective(pos)} #{correct_spelling(word)}."
+        end
+      end
 
-      # Prefix nouns with adjectives, and convert to sentences.
-      phrases.map! { |phrase| correct_spelling(phrase) }
-      phrases.map! { |phrase| "#{adjective} #{phrase}." }
+      # Add emotion words.
+      translated_phrases_with_emotions = []
+      translated_phrases.each_slice(3) do |slice|
+        translated_phrases_with_emotions << slice << emotional_summary
+      end
 
-      # Append a word or phrase describing emotion.
-      phrases << "#{emotional_summary}."
-
-      phrases.join(' ')
+      translated_phrases_with_emotions.flatten.join(' ')
     end
 
-    sentences.join(' ')
+    output = sentences.delete_if {|s| s.nil? || s.empty?}.join(' ')
+    output.empty? ? emotional_summary : output
   end
 
   private
 
-  attr_reader :tagger
+  attr_accessor :adjectives, :emotions, :pos_adjectives, :tagger
+
+  def adjective(part_of_speech)
+    @pos_adjectives[part_of_speech] ||= []
+    @pos_adjectives[part_of_speech] += ADJECTIVE_MAP[part_of_speech] if @pos_adjectives[part_of_speech].empty?
+    @adjectives += ADJECTIVES if @adjectives.empty?
+    determine_unused_adjective(@adjectives, @pos_adjectives[part_of_speech]) || @pos_adjectives[part_of_speech].sample
+  end
+
+  def determine_unused_adjective(overall_adjs, part_of_speech_adjs)
+    return nil if part_of_speech_adjs.empty?
+    shared_adjs = overall_adjs & part_of_speech_adjs
+    random_adj = shared_adjs.sample
+    if random_adj.nil?
+      nil
+    elsif overall_adjs.delete(random_adj)
+      part_of_speech_adjs.delete(random_adj)
+    else
+      determine_unused_adjective(overall_adjs, part_of_speech_adjs  - [random_adj])
+    end
+  end
 
   def correct_spelling(word)
     word.dup.tap do |word|
@@ -49,27 +94,20 @@ class Dogeify
     end
   end
 
-  def ignore_patterns(sentence, patterns)
-    sentence.dup.tap do |sentence|
-      Array(patterns).map { |pattern| sentence.gsub!(pattern, '') }
-    end
-  end
-
-  def adjective
-    ADJECTIVES[adjective_offset]
-  end
-
-  def adjective_offset
-    @adjective_offset ||= -1
-    @adjective_offset = (@adjective_offset + 1) % ADJECTIVES.size
-  end
+  def emotion
+    @emotions += EMOTIONS if @emotions.empty?
+    @emotions.delete(@emotions.sample)
+  end 
 
   def emotional_summary
-    EMOTIONS[emotional_summary_offset]
+    "#{emotion}."
   end
 
-  def emotional_summary_offset
-    @emotional_summary_offset ||= -1
-    @emotional_summary_offset = (@emotional_summary_offset + 1) % EMOTIONS.size
+  def ignore_patterns(sentence, patterns)
+    sentence.scan(/['\-\w]+/).delete_if {|word| patterns.include?(word.downcase)}.join(" ")
+  end
+
+  def part_of_speech(tag)
+    TAGS[tag.to_sym]
   end
 end
